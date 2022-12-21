bb49ae3f62fab7c7983cc4257e60877979dd30b5
Utku <mutkudoganay@gmail.com>
2022-12-21 14:37:28 +0300

    remove unused member

--- pwm.h
+++ pwm.h
@@ -10,11 +10,8 @@
 
 #define PWM_MAX_DUTY 1023
 
-double _pwmFreq = 0;
-
 void pwm_set_freq(double freq) {
     PR2 = (unsigned char) ((_XTAL_FREQ / (freq * 4 * TMR2PRESCALE)) - 1);
-    _pwmFreq = freq;
 }
 
 unsigned int pwm_calculate_duty(double duty) {
